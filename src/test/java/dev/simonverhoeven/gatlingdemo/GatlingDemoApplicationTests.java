9ff5a138c311003fc2f43682be8056fcad5e83d1
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-08-09 19:04:09 +0200

    feat: initial commit

---
+++ src/test/java/dev/simonverhoeven/gatlingdemo/GatlingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simonverhoeven.gatlingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatlingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
