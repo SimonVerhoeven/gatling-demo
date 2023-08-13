aaefab962558a3e32d0936f1f17f1329ff35b68b
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-08-13 11:47:25 +0200

    feat: add chainbuilder setup

--- src/test/java/dev/simonverhoeven/gatlingdemo/GreetingSimulation.java
+++ src/test/java/dev/simonverhoeven/gatlingdemo/GreetingSimulation.java
@@ -1,15 +1,12 @@
 package dev.simonverhoeven.gatlingdemo;
 
-import io.gatling.javaapi.core.CoreDsl;
-import io.gatling.javaapi.core.FeederBuilder;
-import io.gatling.javaapi.core.ScenarioBuilder;
-import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import java.time.Duration;
 import java.util.UUID;
 
-import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 
@@ -19,7 +16,7 @@ public class GreetingSimulation extends Simulation {
             .acceptHeader("application/json")
             .userAgentHeader("Gatling performance Test");
 
-    ScenarioBuilder scenario = CoreDsl.scenario("Load Test greeting")
+    ScenarioBuilder sampleScenario = scenario("Load Test greeting")
             .exec(http("get greeting")
                     .get(session -> "/greet/" + UUID.randomUUID())
                     .check(status().is(200))
@@ -30,8 +27,21 @@ public class GreetingSimulation extends Simulation {
                     .check(status().is(200))
             );
 
+    ChainBuilder greeting = exec(http("get greeting")
+            .get(session -> "/greet/" + UUID.randomUUID())
+            .check(status().is(200))
+    )
+            .pause(5);
+
+    ChainBuilder slowcall = exec(http("Randomly slow")
+            .get("/slow")
+            .check(status().is(200))
+    );
+
+    ScenarioBuilder sampleScenario2 = scenario("Loead test greeting").exec(greeting, slowcall);
+
     public GreetingSimulation() {
-        this.setUp(scenario.injectOpen(constantUsersPerSec(100).during(Duration.ofSeconds(60))))
+        this.setUp(sampleScenario.injectOpen(constantUsersPerSec(100).during(Duration.ofSeconds(60))))
                 .protocols(httpProtocol);
     }
 }
